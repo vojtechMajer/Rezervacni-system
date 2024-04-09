9d15be0151d5e7b344ae16ff57c590e7e0f3cc34
David Lyko <d.lyko.st@spseiostrava.cz>
2024-04-09 13:14:34 +0200

    uprava databaze

--- database.sql
+++ database.sql
@@ -1,45 +1,45 @@
 CREATE TABLE `user_type` (
   `id_user_type` integer PRIMARY KEY,
-  `name` varchar(255)
+  `name` varchar(255) NOT NULL
 );
 
 CREATE TABLE `users` (
   `id_user` integer PRIMARY KEY,
-  `id_user_type` integer,
-  `username` varchar(255),
-  `password` varchar(255)
+  `id_user_type` integer NOT NULL,
+  `username` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL
 );
 
 CREATE TABLE `order` (
   `id_order` integer PRIMARY KEY,
-  `id_reservation` integer,
-  `id_user` integer,
-  `price` integer
+  `id_reservation` integer NOT NULL,
+  `id_user` integer NOT NULL
 );
 
 CREATE TABLE `announcement` (
   `id_announcement` integer PRIMARY KEY,
-  `id_user` integer,
+  `id_user` integer NOT NULL,
   `text` varchar(255)
 );
 
+-- pozor na zmenu date na datime
+
 CREATE TABLE `reservation` (
   `id_reservation` integer PRIMARY KEY,
-  `id_reservation_type` integer,
-  `id_lane` integer,
-  `start` time,
-  `end` time,
-  `date` date
+  `id_reservation_type` integer NOT NULL,
+  `id_lane` integer NOT NULL,
+  `start` datetime NOT NULL,
+  `end` datetime NOT NULL
 );
 
 CREATE TABLE `reservation_type` (
   `id_reservation_type` integer PRIMARY KEY,
-  `name` varchar(255)
+  `name` varchar(255) NOT NULL
 );
 
 CREATE TABLE `lane` (
   `id_lane` integer PRIMARY KEY,
-  `gates` boolean
+  `gates` boolean NOT NULL
 );
 
 ALTER TABLE `announcement` ADD FOREIGN KEY (`id_user`) REFERENCES `users` (`id_user`);
