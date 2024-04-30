4b8ed64767886bd9843429f42faa028c8fb7279b
Novo <novo-gaming@seznam.cz>
2024-04-30 13:48:53 +0200

    .js + .css = ?! it works?

--- JSFunctions/Nav.js
+++ JSFunctions/Nav.js
@@ -1,10 +1,12 @@
 $(function(){
     $(window).scroll(function(){
       var position = $(this).scrollTop();
-      if(position >= 200) {
-        $('.navbar').addClass('exNavbar');
-      } else {
+      if(position >= 215) {
+        $('.navbar').addClass('conNavbar');
         $('.navbar').removeClass('exNavbar');
+      } else {
+        $('.navbar').addClass('exNavbar');
+        $('.navbar').removeClass('conNavbar');
       }
     });
   });
