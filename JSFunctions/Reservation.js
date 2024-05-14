bdf7d6c712b471e35061c1b38d5fdadbf68391e6
Novo <novo-gaming@seznam.cz>
2024-05-14 14:00:04 +0200

    Date input + generating time table

--- JSFunctions/Reservation.js
+++ JSFunctions/Reservation.js
@@ -1,14 +1,16 @@
-$(function() {
-    $("table").each(function() {
-      table = $(this)
-      table.before("<div></div>")
-      table.find("thead th").each(function(index){ 
-        pomocna = $(this).text();
-        table.prev().append("<button>" + pomocna + "</button>")
-      });
-    });
-    $("button").on("click", function() {
-          papousek = $(this).index()+1;
-          $(this).parent().next().find("th:nth-child("+papousek+"),td:nth-child("+papousek+")").toggle()
-        });
-  });
+let list = ["Line 1","Line 2","Line 3"];
+
+$(function(){
+  $("input#SetDate").on("change", function(){
+    if(this.value != "") {
+      console.log(this.value);
+    } else {
+      console.log(Date());
+      this.value = Date()
+    }
+  })
+  list.forEach(function(item){
+    console.log(item)
+  })  
+});
+  
