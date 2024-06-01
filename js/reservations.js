735a9f03a7446cf12e0593c72b5995de29988587
Vojtěch Majer <voj.majer@gmail.com>
2024-06-01 13:10:24 +0200

    Refactor kodu

--- js/reservations.js
+++ js/reservations.js
@@ -2,141 +2,180 @@ $(() => {
     // URL pro JSON return
     let drahyUrl = "http://localhost/lanes";
     //kontrola existence tabulky v pohledu
-
     let reservationTable = $("table#reservation-table");
 
-    let casy = new Map();
-    let reservedTimes = [];
-
+    let timeIntervals = new Map();
+    let reservedTimes = new Map();
+    let orderedTimesHashMap = new Map();
 
     if (reservationTable.length === 0) {
         console.log("Table not found!");
         return;
     }
 
+    // po 30 minutových intervalech
+    // start     ..       end
+    // 8:30 9:00 .. 17:30 18:00
+    let startH = 8;
+    let startM = "00";
+    let endH = 18;
+    let endM = "30";
+
+    // + jeden časový interval aby se vypsal i poslední čas v cyklu 
+    endH += (endM == "30") ? 1 : 0;
+    endM = (endM == "30") ? "00" : "30";
+
+    let hours = startH;
+    let minutes = startM;
+
     let tableHeader = $("<tr>");
     tableHeader.appendTo(reservationTable);
 
-    // start     ..       end
-    // 8:00 8:30 .. 17:30 18:00
-    let start = 8;
-    let end = 18;
-    let countOfTimeIntervals = end - start;
-    tableHeader.append("<th> DRAHY\n/ČASY");
+    // nazev sloupců a řádků
+    tableHeader.append("<th>DRAHY\n/ČASY");
 
-    // 2 kvůli půlhodinovým intervalům
-    for (let i = 0; i <= countOfTimeIntervals; i++) {
-        tableHeader.append("<th> " + String((start + i)) + ":00");
+    while (hours != endH || minutes != endM) {
+        // console.log(hours + ":" + minutes);
 
-        // nebude dávat 18:30
-        if (i != countOfTimeIntervals)
-            tableHeader.append("<th> " + String((start + i)) + ":30");
+        tableHeader.append("<th> " + hours + ":" + minutes);
+        // 17:30 -> 18:00
+        if (minutes == "30") {
+            minutes = "00";
+            hours++;
+        }
+        // 17:00 -> 17:30
+        else {
+            minutes = "30";
+        }
     }
 
-    // Get all lanes and create reservations
-    $.getJSON(drahyUrl, function (result) {
-        $.each(result, function (i, lane) {
-            // let laneContent = reservationTable.append("<tr>");
-            console.log(i, lane);
-            let laneContent = $("<tr>").appendTo(reservationTable);
-            // Row description 
-            laneContent.append("<td> Lane " + lane.id + ":");
+    // Create table
+    $.getJSON(drahyUrl, function (lanes) {
 
-            for (let i = 0; i <= countOfTimeIntervals; i++) {
-                let idH = String(i + start) + "00" + lane.id;
-                let idM = String(i + start) + "30" + lane.id;
-                $("<td>").appendTo(laneContent).attr("id", idH).append("<button>HUH").on("click", function () {
-                    addToReservations(casy, idH, lane.id, reservedTimes);
-                });
+        $.each(lanes, function (i, lane) {
+            console.log(lane.id);
+            // create new table row
+            let laneRow = $("<tr>");
+            // append it to table
+            laneRow.appendTo(reservationTable);
+            // append lane number as first value in row
+            laneRow.append("<th>" + lane.id);
 
-                if (i != countOfTimeIntervals)
-                    $("<td>").appendTo(laneContent).attr("id", idM).append("<button>HUH").on("click", function () {
-                        addToReservations(casy, idM, lane.id, reservedTimes);
-                    });
+            // reset hours and minutes
+            hours = startH;
+            minutes = startM;
+            while (hours != endH || minutes != endM) {
+                // creat table cell
+                let timeInterval = $("<td>");
+                let id = hours + "" + minutes + "" + lane.id;
+
+                $(timeInterval).attr("id", id);
+                // TODO:
+                // make css for the button
+                let button = $("<button>");
+                button.text("HUH");
+
+                button.on("click", function () {
+                    addToReservations($(this).parent(), reservedTimes, orderedTimesHashMap);
+                })
+
+                // append button to cell
+                button.appendTo(timeInterval);
+                // append whole cell with button to row
+                timeInterval.appendTo(laneRow);
+
+                // set timeInterval in HashMap
+                // 8301 -> <td> v čase  8:30 na dráze 1
+                // 9002 -> <td> v čase 9:00 na dráze 2
+                timeIntervals.set(id, timeInterval);
+
+                if (minutes == "30") {
+                    minutes = "00";
+                    hours++;
+                }
+                else {
+                    minutes = "30";
+                }
             }
-
-        });
+        })
+        console.log(timeIntervals);
     }).fail(function (textStatus, errorThrown) {
         console.log("Error fetching data: " + textStatus + errorThrown);
-    });
+    });;
 
     var d = new Date();
     var today = d.getFullYear() + "-" + (d.getMonth() + 1) + "-" + d.getDate();
-    // TODO:
-    // změna data podle výběru uživatele
 
-    // Get reservations from today 
+    // disable all buttons which are already reserved and make fill hashmap with already reserved times 
     $.getJSON("http://localhost/reservations/by-day?day=" + today, function (reservations) {
+
         $.each(reservations, function (i, reservation) {
 
-            // start time
-            let Shours = Number(reservation.startDate.substring(11, 13));
-            let Sminutes = reservation.startDate.substring(14, 16);
+            let reservationStartH = Number(reservation.startDate.substring(11, 13));
+            let reservationStartM = reservation.startDate.substring(14, 16);
 
-            let Ehours = Number(reservation.endDate.substring(11, 13));
-            let Eminutes = reservation.endDate.substring(14, 16);
+            let reservationEndH = Number(reservation.endDate.substring(11, 13));
+            let reservationEndM = reservation.endDate.substring(14, 16)
 
-            reservedTimes.push($("table#reservation-table tr td#" + Shours + "" + Sminutes + "" + reservation.lane.id));
-            reservedTimes.push($("table#reservation-table tr td#" + Ehours + "" + Eminutes + "" + reservation.lane.id));
+            reservationEndH += (reservationEndM == "30") ? 1 : 0;
+            reservationEndM = (reservation.endDate.substring(14, 16) == "30") ? "00" : "30"
 
-            if (Sminutes == "00") {
-                // console.log(Shours + "30" + reservation.lane.id)
-                // console.log(Shours + 1 + "00" + reservation.lane.id)
-                reservedTimes.push($("table#reservation-table tr td#" + Shours + "30" + reservation.lane.id));
-                reservedTimes.push($("table#reservation-table tr td#" + (Shours + 1) + "00" + reservation.lane.id));
+            hours = reservationStartH;
+            minutes = reservationStartM;
 
+            while (hours != reservationEndH || minutes != reservationEndM) {
+                // console.log(hours + ":" + minutes);
+
+                let key = hours + "" + minutes + reservation.lane.id;
+                let cell = timeIntervals.get(key);
+                let button = $(cell).children();
+
+                button.text("KYS");
+                button.css('color', 'red');
+                button.prop('disabled', true);
+
+                reservedTimes.set(key, reservations[i]);
+
+                // 17:30 -> 18:00
+                if (minutes == "30") {
+                    minutes = "00";
+                    hours++;
+                }
+                // 17:00 -> 17:30
+                else {
+                    minutes = "30";
+                }
             }
-            for (let i = 1; i <= Ehours - Shours; i++) {
-                reservedTimes.push($("table#reservation-table tr td#" + (Shours + i) + "00" + reservation.lane.id));
-                if (i != Ehours - Shours)
-                    reservedTimes.push($("table#reservation-table tr td#" + (Shours + i) + "30" + reservation.lane.id));
-
-                $("table#reservation-table tr td#" + (Shours + i) + "30" + reservation.lane.id);
-            }
-
-            $.each(reservedTimes, function (i, time) {
-                time.children().text("KYS");
-                time.children().css('color', 'red');
-                time.children().prop('disabled', true);
-            })
-
-            // console.log(date);
-            //   let date = new Date(reservation.startDate.substring(0, 4), reservation.startDate.substring(5, 7) - 1, reservation.startDate.substring(8, 10));
         })
-
     }).fail(function (textStatus, errorThrown) {
         console.log("Error fetching data: " + textStatus + errorThrown);
-    });
+    });;
 });
 
+function addToReservations(tableCell, reservedTimesHashMap = Map, orderedTimesHashMap = Map) {
 
+    let key = $(tableCell).attr("id");
+    if (reservedTimesHashMap.has(key))
+        return;
 
-function addToReservations(reservations = [], id, line, reservedTimes) {
-    let timeIsOccupied;
+    console.log(key);
+    if (orderedTimesHashMap.has(key)) {
+        // deselect
+        orderedTimesHashMap.delete(key);
 
-    $(reservedTimes).each(function (i, reservedTime) {
-        console.log(reservedTime.attr("id"));
-        if (id === reservedTime.attr("id")) {
-            timeIsOccupied = true;
-        }
-    })
+        // Mark deselected
+        tableCell.children().css('color', 'black');
+        tableCell.children().text("HUH");
 
-    if (!timeIsOccupied) {
-        let element = $("#" + id);
-        if (!reservations.has(id)) {
-            reservations.set(id, line);
-            console.log("Added item to list");
+        console.log("removed from order");
 
-            element.children().css('color', 'yellow');
-            element.children().text("KYS");
-        }
-        else {
-            console.log("This item already exists");
-            reservations.delete(id);
-            element.children().css('color', 'black');
-            element.children().text("HUH");
-        }
+    } else {
+        orderedTimesHashMap.set(key, tableCell);
 
+        // Mark selected
+        tableCell.children().css('color', 'yellow');
+        tableCell.children().text("KYS");
+
+        console.log("added to order");
     }
-    console.log(reservations);
 }
